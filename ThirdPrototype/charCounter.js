b78760b46a1ea749d1fc2683a5bdfc677013f44c
OwenDebenham <Owen.Debenham.2014@live.rhul.ac.uk>
2016-03-15 21:19:51 +0000

    Added third prototype without differently coloured buttons

---
+++ ThirdPrototype/charCounter.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('input#msg').on('keyup',function(){
+		var charLimit = 40;
+		var charCount = $(this).val().replace(/\s/g, '').length;
+		var actualChars = charLimit - charCount;
+		if(charCount > 40) {
+			$("#charLength").css('background-color', 'red');
+			$("#charLength").text(-actualChars + " too many characters");
+		} else { 
+			$("#charLength").css('background-color', '#f0ad4e');
+			$("#charLength").text(actualChars + " characters remaining");
+		}
+	});
+});
